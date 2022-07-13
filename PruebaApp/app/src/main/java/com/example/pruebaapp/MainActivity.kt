83ca3f1b76d96ba9d48f861d88f663e2d2e62af9
hipolito.cristhian28@hotmail.com <C74989975r*>
2022-07-12 22:25:40 -0500

    este es un prueba

--- PruebaApp/app/src/main/java/com/example/pruebaapp/MainActivity.kt
+++ PruebaApp/app/src/main/java/com/example/pruebaapp/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //Este será una creación de prueba
         //Para el git en reunion
+        //prueba 2
     }
 }
