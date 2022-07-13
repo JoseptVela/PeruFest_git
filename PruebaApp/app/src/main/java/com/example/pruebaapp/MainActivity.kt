175f47b907f08f2857697c87dcbdea7af19e7c5a
Josept <hvelab3e@gmail.com>
2022-07-12 22:09:29 -0500

    Hice cambios en main

--- PruebaApp/app/src/main/java/com/example/pruebaapp/MainActivity.kt
+++ PruebaApp/app/src/main/java/com/example/pruebaapp/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //pipipi
+        //Este será una creación de prueba
+        //Para el git en reunion
     }
 }
